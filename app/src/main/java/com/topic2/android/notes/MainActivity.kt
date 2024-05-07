facba231176dad70b0574a77af9a21b3b0375e53
Xhoucw <120392315+pussy-missed@users.noreply.github.com>
2024-05-07 14:08:48 +0300

    AppDrawer finished and Note

--- app/src/main/java/com/topic2/android/notes/MainActivity.kt
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.topic2.android.notes
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
 import com.topic2.android.notes.viewmodel.MainViewModel
 import com.topic2.android.notes.viewmodel.MainViewModelFactory
-
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.rememberCoroutineScope
+import com.topic2.android.notes.routing.Screen
+import com.topic2.android.notes.theme.NotesTheme
+import kotlinx.coroutines.launch
+import ui.components.AppDrawer
+import ui.components.Note
 /**
  * Main activity приложения.
  */
@@ -19,11 +28,34 @@ class MainActivity : AppCompatActivity() {
     )
   })
 
+  @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     setContent {
+      NotesTheme {
+        val coroutineScope = rememberCoroutineScope()
+        val scaffoldState: ScaffoldState = rememberScaffoldState()
+        Scaffold(
+          scaffoldState = scaffoldState,
+          drawerContent = {
+            AppDrawer(
+              currentScreen = Screen.Notes,
+              closeDrawerAction = {
+                coroutineScope.launch {
+                  scaffoldState.drawerState.close()
+                }
+              }
+            )
 
+          },
+          content = {
+            Note()
+          }
+        )
+
+
+      }
     }
   }
 }
