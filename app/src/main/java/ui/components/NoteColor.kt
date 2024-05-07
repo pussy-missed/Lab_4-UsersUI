8b5e2d61674906dfc26371783efb74206d9dc0b0
Xhoucw <120392315+pussy-missed@users.noreply.github.com>
2024-05-07 14:22:35 +0300

    Create NoteColor

---
+++ app/src/main/java/ui/components/NoteColor.kt
@@ -0,0 +1,23 @@
+package ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NoteColor(){
+    Box(
+        modifier = Modifier.size(40.dp).background(Color.Red)
+    )
+}
+
+@Preview
+@Composable
+fun NoteColorPreview(){
+    NoteColor()
+}
