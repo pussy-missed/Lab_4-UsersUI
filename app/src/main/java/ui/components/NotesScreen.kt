87a5fa930285cd342b9ab5e689318ece46135e20
Xhoucw <120392315+pussy-missed@users.noreply.github.com>
2024-05-07 18:32:02 +0300

    Notes in the NoteScreen

--- app/src/main/java/ui/components/NotesScreen.kt
+++ app/src/main/java/ui/components/NotesScreen.kt
@@ -1,12 +1,38 @@
 package ui.components.ColorItem
-
 import androidx.compose.runtime.Composable
 import com.topic2.android.notes.viewmodel.MainViewModel
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import com.topic2.android.notes.ui.components.TopAppBar
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import com.topic2.android.notes.domain.model.NoteModel
+import ui.components.Note
 
 @Composable
 fun NotesScreen(
     viewModel: MainViewModel
 )
 {
+    val notes: List<NoteModel> by viewModel
+        .notesNotInTrash
+        .observeAsState(listOf())
+    Column {
+        TopAppBar(title = "Заметки" ,
+            icon = Icons.Filled.List,
+            onIconClick ={}
+        )
 
+        LazyColumn {
+            items(count = notes.size) { noteIndex -> val note = notes[noteIndex]
+                Note(
+                    note = note,
+                    onNoteClick = {viewModel.onNoteClick(it)},
+                    onNoteCheckedChange = {viewModel.onNoteCheckedChange(it)}
+                )
+            }
+        }
+    }
 }
